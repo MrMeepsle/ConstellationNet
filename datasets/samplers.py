29f09919f28717a6105f78c92ed7fa7d23f1f22e
MrMeepsle <mynameismailtje@gmail.com>
2022-04-04 10:16:46 +0200

    Tried different approach for data sampler

--- datasets/samplers.py
+++ datasets/samplers.py
@@ -7,13 +7,16 @@ class CategoriesSampler():
     def __init__(self, label, n_batch, n_cls, n_per, ep_per_batch=1):
         self.n_batch = n_batch
         self.n_cls = n_cls
-        self.n_per = n_per
         self.ep_per_batch = ep_per_batch
 
         label = np.array(label)
         self.catlocs = []
         for c in range(max(label) + 1):
             self.catlocs.append(np.argwhere(label == c).reshape(-1))
+
+        self.n_per = min(self.catlocs, key=len)  # Account for small datasets
+        if self.n_per > n_per:
+            self.n_per = n_per
 
     def __len__(self):
         return self.n_batch
@@ -25,10 +28,7 @@ class CategoriesSampler():
                 episode = []
                 classes = np.random.choice(len(self.catlocs), self.n_cls, replace=False)
                 for c in classes:
-                    if len(self.catlocs[c]) >= self.n_per:
-                        l = np.random.choice(self.catlocs[c], self.n_per, replace=False)
-                    else:
-                        l = np.random.choice(self.catlocs[c], len(self.catlocs[c]), replace=False)
+                    l = np.random.choice(self.catlocs[c], self.n_per, replace=False)
                     episode.append(torch.from_numpy(l))
                 episode = torch.stack(episode)
                 batch.append(episode)
